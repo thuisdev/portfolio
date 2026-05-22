e2936aa59ec79a4843c6977ad807c3b5bf8c3d38
Adrian <adrian.thuis04@gmail.com>
2026-05-22 23:52:45 +0200

    fixed path .png

--- client/src/components/ProjectsData.tsx
+++ client/src/components/ProjectsData.tsx
@@ -9,7 +9,7 @@ export const ProjectsData = [
     projectGithubUrl: "https://github.com/thuisdev"
   },
   {
-    projectImgSrc: "/public/projects/mood-todo.png",
+    projectImgSrc: "/projects/mood-todo.png",
     projectDemoLink: "https://mood-based-todo-app-zeta.vercel.app/",
     projectTitle: "Mood-based-todo-app",
     projectTitle2: "Todo-app",
